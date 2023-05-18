8f065d9dc2bc4368855361f04a3dbfe9566e293f
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-05-18 14:18:44 +0200

    20230518

--- my_app/src/pages/order-main/order-main.component.ts
+++ my_app/src/pages/order-main/order-main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -13,62 +13,37 @@ export class OrderMainComponent implements OnInit {
   piadinasData: any[] = [];
   saladsData: any[] = [];
   soupsData:any[] = [];
-  pastasData:any[] = []
+  pastasData:any[] = [];
+  error: string = '';
 
+  scrollTo(element: HTMLElement): void {
+      element.scrollIntoView({ behavior: 'smooth' , block: 'start'});
+  }
+ 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.loadPizzaData()
-    this.loadDrinkData()
-    this.loadDessertData()
-    this.loadPiadinasData()
-    this.loadSaladsData()
-    this.loadSoupsData()
-    this.loadPastasData()
+    this.loadData()
+  }
+  loadData() {
+    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(
+      (data) => {
+        this.pizzasData = data.filter((item) => item.option === 'Pizza');
+        this.drinksData = data.filter((item) => item.option === 'Ital');
+        this.dessertsData = data.filter((item) => item.option === 'Desszert');
+        this.piadinasData = data.filter((item) => item.option === 'Piadina');
+        this.saladsData = data.filter((item) => item.option === 'Salata');
+        this.soupsData = data.filter((item) => item.option === 'Leves');
+        this.pastasData = data.filter((item) => item.option === 'Teszta');
+        console.log(this.pastasData)
+      },
+      (error) => {
+        this.error = 'Hiba történt az adatok betöltése közben.';
+        console.error(error);
+      }
+    );
   }
 
-  loadPizzaData() {
-    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      this.pizzasData = data.filter(item => item.option === 'Pizza');
-      console.log(this.pizzasData)
-    });
-  }
-
-  loadDrinkData() {
-    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      this.drinksData = data.filter(item => item.option === 'Ital');
-    });
-  }
-
-  loadDessertData() {
-    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      this.dessertsData = data.filter(item => item.option === 'Desszert');
-    });
-  }
-
-  loadPiadinasData() {
-    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      this.piadinasData = data.filter(item => item.option === 'Piadina');
-    });
-  }
-
-  loadSaladsData() {
-    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      this.saladsData = data.filter(item => item.option === 'Salata');
-    });
-  }
-
-  loadSoupsData() {
-    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      this.soupsData = data.filter(item => item.option === 'Leves');
-    });
-  }
-
-  loadPastasData() {
-    this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      this.pastasData = data.filter(item => item.option === 'Teszta');
-    });
-  }
 
 
 }
